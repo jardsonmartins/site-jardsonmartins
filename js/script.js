98a31d20edd2bc46e0ee65993352bbc6b2e50bd6
Jardson Martins <jardsonmartins12@gmail.com>
2023-05-18 21:16:41 -0300

    update slide customers

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ AOS.init({
     delay: 100,
     once: true
 });
+
+
+// Swiper
+var swiper = new Swiper(".slide-customers", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.customers .container .top .arrows .swiper-button-next',
+        prevEl: '.customers .container .top .arrows .swiper-button-prev',
+    },
+});
