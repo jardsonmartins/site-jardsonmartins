3399831c4086ce7a4f626c9e6e401d28917169f0
Jardson Martins <jardsonmartins12@gmail.com>
2023-06-22 19:58:30 -0300

    update menu-mobile

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-//menu
-const header = document.getElementById('js-header');
-function fixedMenu(){
-    if(window.pageYOffset > 80){
-        header.classList.add('fixed-menu');
-    }else{
-        header.classList.remove('fixed-menu');
-    }
-}
+//Menu mobile
+const btnMenu = document.getElementById('js-btn-menu-mobile');
+const overlayMenu = document.querySelector('.js-overlay');
 
-document.addEventListener('scroll', fixedMenu);
+function openMenuMobile(){
+    document.documentElement.classList.toggle('menu-opened');
+}
+btnMenu.addEventListener('click', openMenuMobile);
+overlayMenu.addEventListener('click', openMenuMobile);
 
 // Biblioteca AOS
 AOS.init({
