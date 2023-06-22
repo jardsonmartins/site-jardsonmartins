89fe7e727e29c5e2bb4b8b43aa7dce761f4f2216
Jardson Martins <jardsonmartins12@gmail.com>
2023-06-22 20:16:36 -0300

    update slides-mobile

--- js/script.js
+++ js/script.js
@@ -19,26 +19,45 @@ AOS.init({
 
 // Swiper
 var swiper = new Swiper(".slide-customers", {
-    slidesPerView: 6,
+    slidesPerView: 2, // Mostra 6 slides por vez no desktop
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
     navigation: {
-        nextEl: '.customers .container .top .arrows .swiper-button-next',
-        prevEl: '.customers .container .top .arrows .swiper-button-prev',
+      nextEl: '.customers .container .top .arrows .swiper-button-next',
+      prevEl: '.customers .container .top .arrows .swiper-button-prev',
     },
+    breakpoints: {
+      // Quando a largura da janela estiver abaixo de 768 pixels
+      768: {
+        slidesPerView: 6, // Mostra 1 slide por vez no mobile
+        spaceBetween: 10,
+      }
+    }
 });
-
+  
 var swiper = new Swiper(".slide-depositions", {
-    slidesPerView: 3,
+    slidesPerView: 1, // Mostra 1 slide por vez
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+      // Quando a largura da janela estiver acima de 900 pixels
+      
+      // Quando a largura da janela estiver entre 768 e 900 pixels
+      576: {
+        slidesPerView: 2, // Mostra 2 slides por vez no desktop
+      },
+      900: {
+        slidesPerView: 3, // Mostra 3 slides por vez no desktop
+      }
+    }
 });
+  
 
 // Fechar details
 var detailsList = document.querySelectorAll(".faq details");
